e9054980416fb66426e43ec60311169268b1c247
HammzaHM <hmemm.hamza1@gmail.com>
2025-01-02 13:37:11 +0100

    chore: remove the (tabs) and get rid of modern yarn related folders

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import "react-native-reanimated";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  Heebo_400Regular,
+  Heebo_600SemiBold,
+  Heebo_700Bold,
+  Heebo_900Black,
+} from "@expo-google-fonts/heebo";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +24,11 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Heebo_400Regular,
+    Heebo_600SemiBold,
+    Heebo_700Bold,
+    Heebo_900Black,
   });
 
   useEffect(() => {
@@ -28,10 +42,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="_contact-me" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ constants/Images.ts
@@ -0,0 +1,4 @@
+export const Images = {
+  myImage: require("../assets/images/my-image.jpg"),
+  bannerImage: require("../assets/images/banner-image.jpg"),
+};
